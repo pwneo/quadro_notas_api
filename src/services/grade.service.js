d111dbdbeadb7b13be817922e80116e2d739f135
Paulo W. A. Ferreira <pauloweskley.dev@gmail.com>
2020-08-11 23:30:43 -0300

    Melhorias na validação do Patch

--- src/services/grade.service.js
+++ src/services/grade.service.js
@@ -1,7 +1,7 @@
 import * as repository from '../repositories/grade.repository.js';
 
 export function save(data){
-    if (!(validateProperties(data) && validateValues(data))){
+    if (!(isPropertiesValidated(data) && isValuesValidated(data) && isNotNullAndNotUndefined(data))){
        throw new Error('Invalid Data');
     }
     return repository.insert(data); 
@@ -20,24 +20,36 @@ export function remove(id){
 }
 
 export function update(id, data){
-    if (!(validateProperties(data) && validateValues(data))){
+    if (!(isPropertiesValidated(data) && isValuesValidated(data) && isNotNullAndNotUndefined(data))){
         throw new Error('Invalid Data');
     }
     return repository.update(id, data);
 }
 
 export function patch(id, data){
-  return repository.patchProperty(id, data);   
+    const propertiesData = Reflect.ownKeys(data);
+    const validValues = isValuesValidated(data, propertiesData);
+    if (!validValues){
+        throw new Error('Invalid Data');
+    }
+    return repository.patchProperty(id, data);   
 } 
 
-function validateValues(data){
-    const notNulAndNotUndefined = repository.getAllProperties()
+
+function isNotNullAndNotUndefined(data){
+    return repository.getAllProperties()
         .every(property => (
             Reflect.get(data, property) !== undefined 
             && Reflect.get(data, property) !== null    
         ));
+}
 
-    const isValid =  repository.getAllProperties()
+function isValuesValidated(data, properties = null){
+    if (properties === null){
+        properties = repository.getAllProperties();
+    }
+
+    return properties
         .every(property => (
                 typeof Reflect.get(data, property) === 'string' 
                 && Reflect.get(data, property) !== ''
@@ -46,11 +58,9 @@ function validateValues(data){
                 typeof Reflect.get(data, property) === 'number' 
                 && Reflect.get(data, property) >= 0
             )
-        );
-   
-    return isValid && notNulAndNotUndefined;
+        );   
 }
 
-function validateProperties(data){
+function isPropertiesValidated(data){
     return repository.getAllProperties().every(property => Reflect.has(data, property));
 }
